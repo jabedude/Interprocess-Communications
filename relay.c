06afb28bdb8980fb766f989e463ed12e00320dff
Josh Abraham <sinisterpatrician@gmail.com>
2017-11-02 22:31:32 -0400

    Move code block in relay.c

--- relay.c
+++ relay.c
@@ -6,11 +6,12 @@
 void unbuf_term(struct termios *savt, struct termios *newt)
 {
     tcgetattr(0, savt);
+
     // Deep copy termios
     memcpy(newt, savt, sizeof(struct termios));
-    for (size_t i = 0; i < sizeof(newt->c_cc); i++) {
+    for (size_t i = 0; i < sizeof(newt->c_cc); i++)
         newt->c_cc[i] = savt->c_cc[i];
-    }
+
     newt->c_lflag &= ~(ICANON | ECHO);
     tcflush(0, TCIFLUSH);
     tcsetattr(0, TCSANOW, newt);
